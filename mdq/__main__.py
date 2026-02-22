5ff33bcaa2c22bcaee4745575a4bc0bb8abc112e
Max Kapur <git@maxkapur.com>
2026-02-22 17:29:48 +0100

    Document command line arguments

--- mdq/__main__.py
+++ mdq/__main__.py
@@ -19,23 +19,59 @@ def main(args: None | list[str] = None) -> None:
         default=[Path()],
         nargs="+",
         type=Path,
+        help="Paths to files or directories to query; defaults to current working directory (see --extensions)",
     )
-    parser.add_argument("-q", "--query", default=None)
     parser.add_argument(
-        "-e", "--extensions", nargs="*", default=["md", "markdown", "txt"]
+        "-q",
+        "--query",
+        default=None,
+        help="Search query",
     )
-    parser.add_argument("-k", "-n", "--n-matches", default=4, type=int)
-    parser.add_argument("--cache-dir", default=None, type=Path)
-    parser.add_argument("--model-name", default="BAAI/bge-small-en-v1.5", type=str)
-    parser.add_argument("--query-prefix", default="query: ", type=str)
+    parser.add_argument(
+        "-e",
+        "--extensions",
+        nargs="*",
+        default=["md", "markdown", "txt"],
+        help="Extensions to match when recursing through a directory",
+    )
+    parser.add_argument(
+        "-k",
+        "-n",
+        "--n-matches",
+        default=4,
+        type=int,
+        help="Number of matches to return",
+    )
+    parser.add_argument(
+        "--cache-dir",
+        default=None,
+        type=Path,
+        help="Directory to store cache of embeddings and downloaded models",
+    )
+    parser.add_argument(
+        "--model-name",
+        default="BAAI/bge-small-en-v1.5",
+        type=str,
+        help="Name of fastembed embedding model",
+    )
+    parser.add_argument(
+        "--query-prefix",
+        default="query: ",
+        type=str,
+        help="Query prefix; default value of 'query: ' can improve performance with default model",
+    )
     parser.set_defaults(func=mdq.query.handle)
 
     # systemd subcommand
     systemd_parser = parser.add_subparsers().add_parser(
-        "systemd", help="Generate systemd service file and enable service"
+        "systemd",
+        help="Generate systemd service file to embed files in current working directory on login",
     )
     systemd_parser.add_argument(
-        "-f", "--force", action="store_true", help="Override existing service file"
+        "-f",
+        "--force",
+        action="store_true",
+        help="Override existing service file",
     )
     systemd_parser.set_defaults(func=mdq.systemd.handle)
 
