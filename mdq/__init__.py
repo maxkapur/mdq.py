2d1ddb5d43635106a0c47b0101c74c7f65ed57ed
Max Kapur <git@maxkapur.com>
2026-02-22 14:54:58 +0100

    Cache the text embedding model itself

--- mdq/__init__.py
+++ mdq/__init__.py
@@ -2,8 +2,11 @@ from fastembed import TextEmbedding
 from platformdirs import user_cache_path
 from rich.console import Console
 
-console = Console(stderr=True)
-conn_path = user_cache_path("mdq", ensure_exists=True) / "cache.db"
+cache_dir = user_cache_path("mdq", ensure_exists=True)
 
-embed_model = TextEmbedding("BAAI/bge-small-en-v1.5")
+console = Console(stderr=True)
+conn_path = cache_dir / "cache.db"
+embed_model = TextEmbedding(
+    "BAAI/bge-small-en-v1.5", cache_dir=cache_dir / "text_embedding"
+)
 query_prefix = "query: "
